48273f126c7fe3e36c5144238f426c229938c450
ANASTAD2000 <anasbaclibr@gmai.com>
2024-12-27 10:23:37 +0100

    add account class

---
+++ src/Account.java
@@ -0,0 +1,7 @@
+// Declaration of the Account class
+public abstract class Account {
+    // Attributes (properties)
+    private String accountNumber;  // Unique account number
+    private double balance;        // Current balance
+    private Client owner;          // Reference to the account owner (Client object)
+}
