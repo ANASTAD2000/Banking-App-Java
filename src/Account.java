3961997c5544c076d9b592106626f27b0d22d099
ANASTAD2000 <anasbaclibr@gmai.com>
2024-12-27 10:25:15 +0100

    Add constructor for Account class

--- src/Account.java
+++ src/Account.java
@@ -3,5 +3,17 @@ public abstract class Account {
     // Attributes (properties)
     private String accountNumber;  // Unique account number
     private double balance;        // Current balance
-    private Client owner;          // Reference to the account owner (Client object)
+    private Client owner; // Reference to the account owner (Client object)
+
+    // Constructor
+    public Account(String accountNumber, double balance, Client owner) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+        this.owner = owner;
+    }
+
+
+
 }
+
+
